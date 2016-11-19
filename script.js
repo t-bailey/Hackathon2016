18e710cdd99a423a4bb4a81a3804bb75b5952e2e
Repkowski <r-repko@onu.edu>
2016-11-19 17:51:58 -0500

    help meeeeeee

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 <<<<<<< HEAD
 
+=======
+>>>>>>> origin/master
 function createCloud() 
 {
 	counts = [5, 4, 2];
 	names = ['a','b','c'];
+<<<<<<< HEAD
 	var MAX_FONT_SIZE = 72;
 }
 
@@ -60,28 +63,42 @@ function rotateElement(result)
 	addWord(names[i],counts[i],max, i);
 	
 }	
+=======
+	var max = Math.max.apply(null, counts);
+	var wordArray = [];
 
-function addWord(word, count, max, position) {
+	for(var i = 0; i < counts.length; i++) {
+		addWord(names[i], counts[i], max, wordArray);
+	}	
+}
+>>>>>>> origin/master
+
+function addWord(word, count, max, wordArray) {
 	var MAX_FONT_SIZE = 72;
 
 	//Get the canvas and context
-	canvas = document.getElementById('cloudCanvas');
-	ctx = canvas.getContext('2d');	
+	var canvas = document.getElementById('cloudCanvas');
+	var ctx = canvas.getContext('2d');	
 
 	//Select the color of the text
 	ctx.fillStyle = getHexColor();
 
 	//Select the size of the word
-	ctx.font = "" + (count / max) * MAX_FONT_SIZE +'pt Ariel';
+	var font_size = (count / max) * MAX_FONT_SIZE;
+	ctx.font = "" + font_size +'pt Ariel';
+
+	//Select the rotation of the word	
+	ctx.save();
+	var rotation = Math.floor(Math.random() * 3) - 1;
 
 	//Select the position of the word
-	var x = getXPosition(position);
-	var y = getYPosition(position);
+	var pos = positionWord(word,font_size,rotation, wordArray);
+	ctx.translate(pos[0], pos[1]);
+	
+	ctx.rotate(rotation * Math.PI/2)	
 
-	//Select the rotation of the word
-
-	//Put the word on the canvas
-	ctx.fillText(word, x, y);
+	ctx.fillText(word, 0, 0);
+	ctx.restore();
 }
 
 function putWordIntoArray(word, font_size, font_type, position)
@@ -109,31 +126,35 @@ function getHexColor() {
 	var r = Math.floor(Math.random() * 256);
 	var g = Math.floor(Math.random() * 256);
 	var b = Math.floor(Math.random() * 256);
-	return '#' + ('0' +r.toString(16)).slice(-2) +  ('0' +g.toString(16)).slice(-2) +  ('0'+ b.toString(16)).slice(-2);
+	return '#' + ('0' + r.toString(16)).slice(-2) +  ('0' + g.toString(16)).slice(-2) +  ('0'+ b.toString(16)).slice(-2);
 }
 
-function getYPosition(position) {
-	return (position+1) * 100;
-}
+function positionWord(word,size,rot,wordArray) {
 
+<<<<<<< HEAD
 function getXPosition(position) {
 	return 10;
-=======
-function createCloud() {
-	counts = [5, 4, 2];
-	names = ['a','b','c'];
-	var MAX_FONT_SIZE = 72;
-
-	canvas = document.getElementById('cloudCanvas');
-	ctx = canvas.getContext('2d');
-
-	max = Math.max.apply(null,counts);
-	console.log(max);
-	for(var i = 0; i < names.length; i++) {
-		ctx.fontColor
-		ctx.font = 'italic ' + (counts[i] / max) * MAX_FONT_SIZE +'pt Calibri';
-		ctx.fillText(names[i], 10, (i+1)*100);
-		console.log('here' + i);
-	}
->>>>>>> origin/master
 }
+=======
+	var canvas = document.getElementById('cloudCanvas');
+	var ctx = canvas.getContext('2d');
+	
+	var width = ctx.measureText(word).width + 5;
+	var height = size + 5;
+
+	if(rot != 0) {
+		var temp = width;
+		width = height;
+		height = temp;
+	}
+
+	var width = 600;
+	var height = 400;
+	x = Math.floor(Math.random() * width) - (width/2);
+	y = Math.floor(Math.random() * height) - (height/2);
+	x2 = x + width;
+	y2 = y + height;
+
+	return [x,y];
+}
+>>>>>>> origin/master
