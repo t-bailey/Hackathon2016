b3a11b417b8973c9a5779eb6fb9e58920847f2a3
Repkowski <r-repko@onu.edu>
2016-11-19 16:29:39 -0500

    Javascript Changes v2

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-<<<<<<< HEAD
+
 function createCloud() 
 {
 	counts = [5, 4, 2];
 	names = ['a','b','c'];
 	var MAX_FONT_SIZE = 72;
-=======
-function createCloud() {
+}
+
+function createCloud() 
+{
 	var counts = [5, 12, 2];
 	var names = ['a','b','c'];
->>>>>>> origin/master
 
 	canvas = document.getElementById('cloudCanvas');
 	ctx = canvas.getContext('2d');
@@ -16,10 +17,11 @@ function createCloud() {
 	drawCloud(names, counts);
 }
 
-function drawCloud(names, counts) {
+function drawCloud(names, counts) 
+{
 	max = Math.max.apply(null,counts);
-	for(var i = 0; i < names.length; i++) {
-<<<<<<< HEAD
+	for(var i = 0; i < names.length; i++) 
+	{
 		ctx.fontColor
 		ctx.font = 'italic ' + (counts[i] / max) * MAX_FONT_SIZE +'pt Calibri';
 		ctx.fillText(names[i], 10, (i+1)*100);
@@ -53,10 +55,10 @@ function rotateElement(result)
 	context.restore();
 	console.log('did the rotation ' + j);
 	
-=======
-		addWord(names[i],counts[i],max, i);
-	}	
-}
+
+	addWord(names[i],counts[i],max, i);
+	
+}	
 
 function addWord(word, count, max, position) {
 	var MAX_FONT_SIZE = 72;
@@ -81,6 +83,27 @@ function addWord(word, count, max, position) {
 	ctx.fillText(word, x, y);
 }
 
+function putWordIntoArray(word, font_size, font_type, position)
+{
+	arrayOfWords = [];
+	canvas = document.getElementById('cloudCanvas');
+	context = canvas.getContext('2d');
+	
+	//Gets the width attribute of the word with a little bit of padding
+	int width = context.measureText(word).width + 5;
+	
+	//Keeps a little room so the text is padded
+	int height = font_size + 5;
+	
+	var wordObject = {txt: word, size: font_size, font: font_type, width: width, height: height, pos: position};
+	arrayOfWords.push(wordObject);
+}
+
+function checkForWord()
+{
+	//Needs to be redone
+}
+
 function getHexColor() {
 	var r = Math.floor(Math.random() * 256);
 	var g = Math.floor(Math.random() * 256);
@@ -94,5 +117,4 @@ function getYPosition(position) {
 
 function getXPosition(position) {
 	return 10;
->>>>>>> origin/master
 }
