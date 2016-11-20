53deeaf161657e08837b51f38222a0c8d18e194d
GenericallyTerrible <jmoehrnkel@gmail.com>
2016-11-20 00:54:57 -0500

    AGRI - culture

--- kappa-cloud.dev/twitch.js
+++ kappa-cloud.dev/twitch.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     var emoteArray = ["4Head","AMPEnergy","AMPEnergyCherry","AMPTropPunch","ANELE","ArgieB8","ArsonNoSexy","AsianGlow","BabyRage","BatChest","BCouch","BCWarrior","BibleThump","BigBrother","BlargNaut","bleedPurple","BloodTrail","BrainSlug","BrokeBack","BudBlast","BuddhaBar","BudStar","ChefFrank","cmonBruh","CoolCat","CoolStoryBob","copyThis","CorgiDerp","CurseLit","DAESuppy","DansGame","DatSheffy","DBstyle","deIlluminati","DendiFace","DogFace","DoritosChip","duDudu","DxAbomb","DxCat","EagleEye","EleGiggle","FailFish","FPSMarksman","FrankerZ","FreakinStinkin","FUNgineer","FunRun","FutureMan","GingerPower","GivePLZ","GOWSkull","GrammarKing","HassaanChop","HassanChop","HeyGuys","HotPokket","HumbleLife","imGlitch","Jebaited","JKanStyle","JonCarnage","KAPOW","Kappa","KappaClaus","KappaPride","KappaRoss","KappaWealth","Keepo","KevinTurtle","Kippa","Kreygasm","Mau5","mcaT","MikeHogu","MingLee","MrDestructoid","MVGame","NerfBlueBlaster","NerfRedBlaster","NervousMonkey","NinjaTroll","NomNom","NoNoSpot","NotATK","NotLikeThis","OhMyDog","OMGScoots","OneHand","OpieOP","OptimizePrime","OSfrog","OSkomodo","OSsloth","panicBasket","PanicVis","PartyTime","pastaThat","PeoplesChamp","PermaSmug","PeteZaroll","PeteZarollTie","PicoMause","PipeHype","PJSalt","PJSugar","PMSTwin","PogChamp","Poooound","PraiseIt","PRChase","PrimeMe","PunchTrees","PuppeyFace","RaccAttack","RalpherZ","RedCoat","ResidentSleeper","riPepperonis","RitzMitz","RuleFive","SeemsGood","SGlemon","SGmouth","SGrasp","ShadyLulu","ShazBotstix","SmoocherZ","SMOrc","SoBayed","SoonerLater","SSSsss","StinkyCheese","StoneLightning","StrawBeary","SuperVinlin","SwiftRage","TakeNRG","TBCheesePull","TBTacoLeft","TBTacoRight","TF2John","TheRinger","TheTarFu","TheThing","ThunBeast","TinyFace","TooSpicy","TriHard","TTours","twitchRaid","TwitchRPG","UleetBackup","UncleNox","UnSane","VoHiYo","VoteNay","VoteYea","WholeWheat","WTRuck","WutFace","YouWHY"];
 	var emoteCountArray = Array.apply(null, Array(emoteArray.length)).map(Number.prototype.valueOf,0);
 
-    ws.onopen = function open() {
+    ws.onopen = function(){
         //ws.send('CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership');
         ws.send('PASS ' + pass);
         ws.send('NICK ' + nick);
@@ -16,8 +16,6 @@ $(document).ready(function(){
         setInterval(function() {
             pong();
             console.log("PONG sent");
-            //console.log(emoteCountArray);
-            
             var response =  getTop(30, emoteArray, emoteCountArray);
             console.log(response[0]);
             console.log(response[1]);
@@ -39,8 +37,7 @@ $(document).ready(function(){
 
     function pong(){
         ws.send('PONG :tmi.twitch.tv');
-    }
-
+    };
 });
 
 function readEmoteFunction(message, emoteArray, emoteCountArray)
@@ -78,15 +75,17 @@ function indexOfMax(arr) {
 function getTop(number, emoteArray, emoteCountArray) {
     var names = new Array();
     var counts = new Array();
+    var ea = JSON.parse(JSON.stringify(emoteArray));
+    var eca = JSON.parse(JSON.stringify(emoteCountArray));
 
     for(var i = 0; i < number; i++) {
-        var index = indexOfMax(emoteCountArray);
-        if(emoteCountArray[index] == 0)
+        var index = indexOfMax(eca);
+        if(eca[index] == 0)
             continue;
         else {
-            names[i] = emoteArray[index];
-            counts[i] = emoteCountArray[index];
-            emoteCountArray[index] = 0;
+            names[i] = ea[index];
+            counts[i] = eca[index];
+            eca[index] = 0;
         }
     }
 
