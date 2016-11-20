9362382f539944c52766dd1a2695159b4690308e
jduffey2 <j-duffey.2@onu.edu>
2016-11-20 06:52:46 -0500

    Channel Choosing
    Added the ability to change the channel

--- kappa-cloud.dev/twitch.js
+++ kappa-cloud.dev/twitch.js
@@ -1,44 +1,52 @@
-$(document).ready(function(){
-    var ws = new WebSocket('ws://irc-ws.chat.twitch.tv:80');
-    var pass = 'oauth:q3yx09tynhaoqu744w9hm3kpree7cs';
-    var nick = 'temporarily_terrible';
-    var channel = 'c9sneaky';
-    var chatCommand = "PRIVMSG";
-
-    var emoteArray = ["4Head","AMPEnergy","AMPEnergyCherry","AMPTropPunch","ANELE","ArgieB8","ArsonNoSexy","AsianGlow","BabyRage","BatChest","BCouch","BCWarrior","BibleThump","BigBrother","BlargNaut","bleedPurple","BloodTrail","BrainSlug","BrokeBack","BudBlast","BuddhaBar","BudStar","ChefFrank","cmonBruh","CoolCat","CoolStoryBob","copyThis","CorgiDerp","CurseLit","DAESuppy","DansGame","DatSheffy","DBstyle","deIlluminati","DendiFace","DogFace","DoritosChip","duDudu","DxAbomb","DxCat","EagleEye","EleGiggle","FailFish","FPSMarksman","FrankerZ","FreakinStinkin","FUNgineer","FunRun","FutureMan","GingerPower","GivePLZ","GOWSkull","GrammarKing","HassaanChop","HassanChop","HeyGuys","HotPokket","HumbleLife","imGlitch","Jebaited","JKanStyle","JonCarnage","KAPOW","Kappa","KappaClaus","KappaPride","KappaRoss","KappaWealth","Keepo","KevinTurtle","Kippa","Kreygasm","Mau5","mcaT","MikeHogu","MingLee","MrDestructoid","MVGame","NerfBlueBlaster","NerfRedBlaster","NervousMonkey","NinjaTroll","NomNom","NoNoSpot","NotATK","NotLikeThis","OhMyDog","OMGScoots","OneHand","OpieOP","OptimizePrime","OSfrog","OSkomodo","OSsloth","panicBasket","PanicVis","PartyTime","pastaThat","PeoplesChamp","PermaSmug","PeteZaroll","PeteZarollTie","PicoMause","PipeHype","PJSalt","PJSugar","PMSTwin","PogChamp","Poooound","PraiseIt","PRChase","PrimeMe","PunchTrees","PuppeyFace","RaccAttack","RalpherZ","RedCoat","ResidentSleeper","riPepperonis","RitzMitz","RuleFive","SeemsGood","SGlemon","SGmouth","SGrasp","ShadyLulu","ShazBotstix","SmoocherZ","SMOrc","SoBayed","SoonerLater","SSSsss","StinkyCheese","StoneLightning","StrawBeary","SuperVinlin","SwiftRage","TakeNRG","TBCheesePull","TBTacoLeft","TBTacoRight","TF2John","TheRinger","TheTarFu","TheThing","ThunBeast","TinyFace","TooSpicy","TriHard","TTours","twitchRaid","TwitchRPG","UleetBackup","UncleNox","UnSane","VoHiYo","VoteNay","VoteYea","WholeWheat","WTRuck","WutFace","YouWHY"];
-	var emoteCountArray = Array.apply(null, Array(emoteArray.length)).map(Number.prototype.valueOf,0);
-
-    ws.onopen = function(){
-        //ws.send('CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership');
-        ws.send('PASS ' + pass);
-        ws.send('NICK ' + nick);
-        ws.send('JOIN #' + channel);
-        setInterval(function() {
-            pong();
-            //console.log("PONG sent");
-            var response =  getTop(40, emoteArray, emoteCountArray);
-            //console.log(response[0]);
-            //console.log(response[1]);
-            drawCloud(response[0], response[1]);
-
-        }, 30000)
-    };
-
-    //show messages
-    ws.onmessage = function(data){
-        var message = data['data'];
-        var msgIndex = message.indexOf(':', 1) + 1;
-        if (msgIndex > 0 && message.includes(chatCommand)){
-            message = message.substring(msgIndex);
-            //console.log(message);
-            emoteCountArray = readEmoteFunction(message, emoteArray, emoteCountArray);
+var interval;
+$(document).ready(function() {
+    $('#channelSubmitBtn').on("click",function(){
+        if(interval != null) {
+            clearInterval(interval);
         }
-    };
+        
+        var ws = new WebSocket('ws://irc-ws.chat.twitch.tv:80');
+        var pass = 'oauth:q3yx09tynhaoqu744w9hm3kpree7cs';
+        var nick = 'temporarily_terrible';
+        var channel = document.getElementById('channelTB').value;//'c9sneaky';
+        var chatCommand = "PRIVMSG";
 
-    function pong(){
-        ws.send('PONG :tmi.twitch.tv');
-    };
+        var emoteArray = ["4Head","AMPEnergy","AMPEnergyCherry","AMPTropPunch","ANELE","ArgieB8","ArsonNoSexy","AsianGlow","BabyRage","BatChest","BCouch","BCWarrior","BibleThump","BigBrother","BlargNaut","bleedPurple","BloodTrail","BrainSlug","BrokeBack","BudBlast","BuddhaBar","BudStar","ChefFrank","cmonBruh","CoolCat","CoolStoryBob","copyThis","CorgiDerp","CurseLit","DAESuppy","DansGame","DatSheffy","DBstyle","deIlluminati","DendiFace","DogFace","DoritosChip","duDudu","DxAbomb","DxCat","EagleEye","EleGiggle","FailFish","FPSMarksman","FrankerZ","FreakinStinkin","FUNgineer","FunRun","FutureMan","GingerPower","GivePLZ","GOWSkull","GrammarKing","HassaanChop","HassanChop","HeyGuys","HotPokket","HumbleLife","imGlitch","Jebaited","JKanStyle","JonCarnage","KAPOW","Kappa","KappaClaus","KappaPride","KappaRoss","KappaWealth","Keepo","KevinTurtle","Kippa","Kreygasm","Mau5","mcaT","MikeHogu","MingLee","MrDestructoid","MVGame","NerfBlueBlaster","NerfRedBlaster","NervousMonkey","NinjaTroll","NomNom","NoNoSpot","NotATK","NotLikeThis","OhMyDog","OMGScoots","OneHand","OpieOP","OptimizePrime","OSfrog","OSkomodo","OSsloth","panicBasket","PanicVis","PartyTime","pastaThat","PeoplesChamp","PermaSmug","PeteZaroll","PeteZarollTie","PicoMause","PipeHype","PJSalt","PJSugar","PMSTwin","PogChamp","Poooound","PraiseIt","PRChase","PrimeMe","PunchTrees","PuppeyFace","RaccAttack","RalpherZ","RedCoat","ResidentSleeper","riPepperonis","RitzMitz","RuleFive","SeemsGood","SGlemon","SGmouth","SGrasp","ShadyLulu","ShazBotstix","SmoocherZ","SMOrc","SoBayed","SoonerLater","SSSsss","StinkyCheese","StoneLightning","StrawBeary","SuperVinlin","SwiftRage","TakeNRG","TBCheesePull","TBTacoLeft","TBTacoRight","TF2John","TheRinger","TheTarFu","TheThing","ThunBeast","TinyFace","TooSpicy","TriHard","TTours","twitchRaid","TwitchRPG","UleetBackup","UncleNox","UnSane","VoHiYo","VoteNay","VoteYea","WholeWheat","WTRuck","WutFace","YouWHY"];
+        var emoteCountArray = Array.apply(null, Array(emoteArray.length)).map(Number.prototype.valueOf,0);
+
+        ws.onopen = function(){
+            //ws.send('CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership');
+            ws.send('PASS ' + pass);
+            ws.send('NICK ' + nick);
+            ws.send('JOIN #' + channel);
+            interval = setInterval(function() {
+                pong();
+                //console.log("PONG sent");
+                var response =  getTop(40, emoteArray, emoteCountArray);
+                //console.log(response[0]);
+                //console.log(response[1]);
+                drawCloud(response[0], response[1]);
+
+            }, 15000)
+        };
+
+        //show messages
+        ws.onmessage = function(data){
+            var message = data['data'];
+            var msgIndex = message.indexOf(':', 1) + 1;
+            if (msgIndex > 0 && message.includes(chatCommand)){
+                message = message.substring(msgIndex);
+                //console.log(message);
+                emoteCountArray = readEmoteFunction(message, emoteArray, emoteCountArray);
+            }
+        };
+
+        function pong(){
+            ws.send('PONG :tmi.twitch.tv');
+        };
+    });
 });
+
 
 function readEmoteFunction(message, emoteArray, emoteCountArray)
 {
